69aa9ae25afdd2a72845ae5ef473325de3737c6a
JoseMatheusR <josematheusrodriguesd@gmail.com>
2024-11-07 21:29:04 -0300

    feat: implementação de endereços, validação de endereços e registro de transações

--- Blockchain.js
+++ Blockchain.js
@@ -1,6 +1,8 @@
 const { format } = require('date-fns');
 const { Block } = require('./Block');
 const {Transaction} = require("./Transaction.js");
+const EC = require ('elliptic').ec
+const ec = new EC('secp256k1')
 
 class Blockchain {
     constructor() {
@@ -8,6 +10,7 @@ class Blockchain {
         this.difficulty = 3;
         this.pendingTransactions = [];
         this.reward = 10;
+        this.addressPattern = /^zezo.{46}$/
     }
 
     createGenesis() {
@@ -21,22 +24,49 @@ class Blockchain {
     minePendingTransactions(rewardAddress) {
         const rewardTransaction = new Transaction(null, this.reward, "Bitcoin", rewardAddress);
         console.log("Mining Block...");
+        this.pendingTransactions.push(rewardTransaction);
 
         let block = new Block(format(Date.now(), 'yyyy-MM-dd HH:mm:ss'), this.pendingTransactions, this.getLastBlock().hash);
         block.mineBlock(this.difficulty);
-
         console.log('Block successfully mined');
+  
         this.chain.push(block);
         this.pendingTransactions = [];
-        this.pendingTransactions.push(rewardTransaction);
-
     }
 
     createTransaction(transaction) {
         if (!transaction.destination || transaction.quantity <= 0) {
             throw new Error('Invalid transaction');
+        } 
+        console.log("validating address...")
+        if(!this.addressPattern.test(transaction.origin) || !this.addressPattern.test(transaction.destination)){
+            throw new Error('Invalid Address');     
         }
         this.pendingTransactions.push(transaction);
+        console.log("Transaction successfully created")
+    }
+
+    createNewAddress(){
+        const key = ec.genKeyPair();
+        return 'zezo' + key.getPublic('hex').slice(0, 46);
+    }
+
+    getUserTransactions(userAddress) {
+        const transactions = [];
+
+        for (const block of this.chain) {
+            for (const tx of block.transactions) {
+                if (tx.origin === userAddress || tx.destination === userAddress) {
+                    transactions.push(tx);
+                }
+            }
+        }
+
+        return transactions;
+    }
+
+    isAddressValid(address){
+        return this.addressPattern.test(address);
     }
 
     isChainValid() {
--- main.js
+++ main.js
@@ -3,17 +3,23 @@ const {Block} = require("./Block.js");
 const {Blockchain} = require("./Blockchain.js");
 
 let testezada = new Blockchain();
+
+let userAddresses = [];
+for(i = 0; i < 10; i++){
+    userAddresses.push(testezada.createNewAddress());
+}
+
 new Transaction();
-testezada.createTransaction(new Transaction("Matheus", 15, "bitcoin", "Sávio"));
-testezada.createTransaction(new Transaction("Bea", 20, "bitcoin", "Thulio"));
-testezada.createTransaction(new Transaction("Thulio", 2, "bitcoin", "Bea"));
-testezada.createTransaction(new Transaction("Samuel", 13, "bitcoin", "Tigrão"));
-testezada.minePendingTransactions("Steve");
-testezada.createTransaction(new Transaction("Wandreus", 16, "bitcoin", "Bruno"));
-testezada.createTransaction(new Transaction("Samuel", 27, "bitcoin", "Tigrão"));
-testezada.minePendingTransactions("Caronte");
-testezada.createTransaction(new Transaction("Isac", 20, "bitcoin", "Wandreus"));
-testezada.minePendingTransactions("Chico Buarque");
+testezada.createTransaction(new Transaction(userAddresses[0], 15, "bitcoin", userAddresses[1]));
+testezada.createTransaction(new Transaction(userAddresses[0], 20, "bitcoin", userAddresses[2]));
+testezada.createTransaction(new Transaction(userAddresses[5], 2, "bitcoin", userAddresses[6]));
+testezada.createTransaction(new Transaction(userAddresses[3], 13, "bitcoin", userAddresses[0]));
+testezada.minePendingTransactions(userAddresses[9]);
+testezada.createTransaction(new Transaction(userAddresses[2], 16, "bitcoin", userAddresses[0]));
+testezada.createTransaction(new Transaction(userAddresses[7], 27, "bitcoin", userAddresses[0]));
+testezada.minePendingTransactions(userAddresses[9]);
+testezada.createTransaction(new Transaction(userAddresses[6], 20, "bitcoin", userAddresses[7]));
+testezada.minePendingTransactions(userAddresses[9]);
 
 testezada.chain.forEach((block, index) => {
     console.log(`\nBlock ${index} \n[Timestamp: ${block.timestamp} \nNounce: ${block.nounce}\nHash: ${block.hash} \nPrevious Hash: ${block.previousHash}]`);
@@ -21,3 +27,8 @@ testezada.chain.forEach((block, index) => {
         console.log(  `Transaction: ${txIndex + 1}: ${tx.origin || 'Mining Reward'} -> ${tx.destination} | ${tx.quantity} ${tx.currency}`);
     });
 });
+console.log("\n\ntransações do úsuario 0:\n")
+console.log(testezada.getUserTransactions(userAddresses[0]));
+
+
+
